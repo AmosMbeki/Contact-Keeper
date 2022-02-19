0251c21583d2444e08c5059653c39e4f85b6b891
AmosMbeki <amosmbeki@gmail.com>
2022-02-19 22:22:52 +0300

    User Interface Implementation completed

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div>
+        <h1>About Contact Keeper</h1>
+        <p className="my-1">
+            Full Stack - React App for Keeping contacts 
+        </p>
+        <p className="bg-dark p">
+            <strong>Version: </strong> @1.0.0
+        </p>
+    </div>
+  )
+}
---
+++ client/src/components/pages/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { ContactForm } from "../contacts/ContactForm";
+import { Contacts } from "../contacts/Contacts";
+import { ContactFilter } from "../contacts/ContactFilter";
+
+
+export const Home = () => {
+  return (
+    <div className="grid-2">
+      <div>
+        <ContactForm />
+      </div>
+      <div>
+        <ContactFilter />
+        <Contacts />
+      </div>
+    </div>
+  )
+}
