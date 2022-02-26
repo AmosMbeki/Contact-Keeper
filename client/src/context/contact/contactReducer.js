a9412e0fdb1c58b6aad2e1e39dcaf4e29b347316
AmosMbeki <amosmbeki@gmail.com>
2022-02-26 15:16:17 +0300

    Connected to backend API

--- client/src/context/contact/contactReducer.js
+++ client/src/context/contact/contactReducer.js
@@ -1,11 +1,14 @@
 import {
     ADD_CONTACT,
+    GET_CONTACTS,
     DELETE_CONTACT,
     SET_CURRENT,
     CLEAR_CURRENT,
     UPDATE_CONTACT,
     FILTER_CONTACTS,
-    CLEAR_FILTER
+    CLEAR_CONTACTS,
+    CLEAR_FILTER,
+    CONTACT_ERROR
 } from '../types';
 
 export default (state, action) => {
@@ -13,20 +16,40 @@ export default (state, action) => {
         case ADD_CONTACT:
             return {
                 ...state,
-                contacts: [...state.contacts, action.payload]
+                contacts: [ action.payload, ...state.contacts],
+                loading: false
             };
+
+        case GET_CONTACTS:
+            return{
+                ...state,
+                contacts: action.payload,
+                loading: false
+            }
             
         case UPDATE_CONTACT:
             return{
                 ...state,
-                contacts: state.contacts.map(contact => contact.id === action.payload.id ? action.payload : contact)
+                contacts: state.contacts.map(contact => contact._id === action.payload._id ? action.payload : contact),
+                loading: false
             }
 
         case DELETE_CONTACT:
             return{
                 ...state,
-                contacts: state.contacts.filter(contact => contact.id !== action.payload)
+                contacts: state.contacts.filter(contact => contact._id !== action.payload),
+                loading: false
             };
+
+        case CLEAR_CONTACTS:
+            return{
+                ...state,
+                contacts: null,
+                filtered: null,
+                error: null,
+                current: null,
+                
+            }
 
         case SET_CURRENT:
             return{
@@ -55,6 +78,11 @@ export default (state, action) => {
                 filtered: null
             };
     
+        case CONTACT_ERROR:
+            return{
+                ...state, 
+                error: action.payload
+            }
 
         default:
             return state;
